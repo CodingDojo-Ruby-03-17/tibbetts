d7cb3dea878b1681c3c0fc0376856dac076bb4eb
Kyle Tibbetts <ktibbetts44@gmail.com>
2017-03-25 23:05:09 +0000

    added Method Testing

---
+++ TDD/MethodTesting/helper.rb
@@ -0,0 +1,15 @@
+module Helpers
+
+  def with_captured_stdout
+    :available
+    begin
+      old_stdout = $stdout
+      $stdout = StringIO.new('','w')
+      yield
+      $stdout.string
+    ensure
+      $stdout = old_stdout
+    end
+  end
+
+end
